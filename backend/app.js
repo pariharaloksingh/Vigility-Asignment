d9ae3e129b262a1cc7b092f9d6b9acca023bb096
alok singh parihar <pariharaloksingh4@gmail.com>
2026-04-19 14:11:17 +0530

    before frontend deployment some cors chages

--- backend/app.js
+++ backend/app.js
@@ -10,7 +10,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: process.env.CLIENT_URL || "http://localhost:5173",
+     origin: "*",
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
