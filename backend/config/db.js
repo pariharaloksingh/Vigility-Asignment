a9ab685c31da451f9d556de64ab9ea0314e89850
alok singh parihar <pariharaloksingh4@gmail.com>
2026-04-19 15:07:18 +0530

    db,js uncommit for backend deploymnet again

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,31 +1,31 @@
-// const { Sequelize } = require("sequelize"); // 👈 MUST
-// require("dotenv").config();
+const { Sequelize } = require("sequelize"); // 👈 MUST
+require("dotenv").config();
 
-// const sequelize = new Sequelize(
-//   process.env.DB_NAME,
-//   process.env.DB_USER,
-//   process.env.DB_PASS,
-//   {
-//     host: process.env.DB_HOST,
-//     dialect: process.env.DB_DIALECT || "postgres",
-//     port: parseInt(process.env.DB_PORT) || 5432,
-//     logging: false,
-//   }
-// );
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASS,
+  {
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT || "postgres",
+    port: parseInt(process.env.DB_PORT) || 5432,
+    logging: false,
+  }
+);
 
-module.exports = sequelize;
+// module.exports = sequelize;
 
-const { Sequelize } = require("sequelize");
+// const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
-  dialect: "postgres",
-  logging: false,
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false,
-    },
-  },
-});
+// const sequelize = new Sequelize(process.env.DATABASE_URL, {
+//   dialect: "postgres",
+//   logging: false,
+//   dialectOptions: {
+//     ssl: {
+//       require: true,
+//       rejectUnauthorized: false,
+//     },
+//   },
+// });
 
-module.exports = sequelize;
+// module.exports = sequelize;
