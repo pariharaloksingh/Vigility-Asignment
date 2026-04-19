902f7efa01396e65f53a5a5ef1570e20aa286d5d
alok singh parihar <pariharaloksingh4@gmail.com>
2026-04-19 13:00:47 +0530

    db file changes

--- backend/config/db.js
+++ backend/config/db.js
@@ -7,8 +7,8 @@ const sequelize = new Sequelize(
   process.env.DB_PASS,
   {
     host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT,
-    port: process.env.DB_PORT,
+    dialect: process.env.DB_DIALECT || "postgres",
+    port: parseInt(process.env.DB_PORT) || 5432,
     logging: false,
   }
 );
